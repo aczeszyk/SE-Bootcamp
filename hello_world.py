9a2558de95faa483c303c216b3accecc77a4984f
Ola Czeszyk <154804387+aczeszyk@users.noreply.github.com>
2023-12-25 10:54:49 +0000

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+#request name input from the user
+#print name input from the user
+#request age input from the user
+#print age input from the user
+#print “Hello World!”
+
+name = input("Enter your name")
+print(name)
+age = input("Enter your age")
+print(age)
+print("Hello World!")
