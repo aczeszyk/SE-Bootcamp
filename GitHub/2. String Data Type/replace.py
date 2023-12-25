521a1c81a1cbab731e7846dd9ff28f22ea977eec
Ola Czeszyk <154804387+aczeszyk@users.noreply.github.com>
2023-12-25 11:23:10 +0000

    Delete GitHub directory

--- GitHub/2. String Data Type/replace.py
+++
@@ -1,5 +0,0 @@
-string1 = "The!quick!brown!fox!jumps!over!the!lazy!dog."
-print(string1.replace("!"," "))
-print(string1.upper().replace("!", " "))
-print(len("The!quick!brown!fox!jumps!over!the!lazy!dog."))
-print(string1[::-1])
